37f2beb6eaed04f19b66ce4b796202a1dc2d03c6
drkenni <tanyromachka1@mail.ru>
2023-05-16 11:42:11 +0300

    Flake8 on 100%

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,4 @@
 from googleapiclient.discovery import build
-import json
 import os
 
 
@@ -7,13 +6,15 @@ class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """Экземпляр инициализируется id канала.
+         Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         api_key: str = os.getenv('YT_API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(
+            id=self.channel_id, part='snippet,statistics'
+        ).execute()
         return channel
-
